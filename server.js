903648acfc68eb76f1285433d56992767f71bba3
Sonu raj <rajjsonu027@gmail.com>
2026-09-14 14:15:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const PORT = 3000;
 
+// Serve HTML, CSS, JS files from current folder
 app.use(express.static(__dirname));
 
-app.listen(3000, "0.0.0.0", () => {
-    console.log("Server running on port 3000");
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
